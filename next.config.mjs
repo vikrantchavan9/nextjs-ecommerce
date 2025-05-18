71e512bcf18cf3b43798478ce71fa96a9548aa60
Vikrant <vikrantchavan200@gmail.com>
2025-05-18 21:21:40 +0530

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+     images: {
+          domains: [
+               'res.cloudinary.com', // Add your Cloudinary domain here
+               // Add any other external image domains you might use (e.g., 'fakestoreapi.com', 'example.com')
+          ],
+     },
+};
 export default nextConfig;
